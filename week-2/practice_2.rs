3284570a62914683dd264be00b2ec24e0d7d4357
t.dikeCOS101 <diketoby67@gmail.com>
2023-10-23 00:59:06 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+	
+	println!(); // prints just a
+
+	println!("hello");//prints
+
+    println!("format {} argument", "some"); //prints format some arguments
+
+    println!("My name is {}. I am the class rep of stream 1", "	Daggo Willaim-Jumbo");
+    
+	}
